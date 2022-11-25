ef459bcd079c43754cc1fa22d363f1ad805afb41
drwadu <dominik.rusovac@hotmail.com>
2022-11-25 17:41:13 +0100

    update.

--- sync_ccg_ucs.py
+++ sync_ccg_ucs.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         l[1]: int(l[0])
         for l in map(
             lambda l: l.rstrip("\n").split(" ")[1:],
-            filter(lambda l: l.startswith("c "), open(sys.argv[2], "r").readlines()),
+            filter(lambda l: l.startswith("c ") and not "projected_away" in l, open(sys.argv[2], "r").readlines()),
         )
     }
 
@@ -25,7 +25,6 @@ if __name__ == "__main__":
         if k_ucs == k_ccg
     }
 
-    # print(ucs_file[0], end="")
     for line in filter(lambda l: not l.startswith("c "), ucs_file[1:]):
         for a in map(int, line.strip(" \n").split(" ")):
             if a > 0:
@@ -33,42 +32,3 @@ if __name__ == "__main__":
             else:
                 print(f"-{maps[abs(a)]}", end=" ")
         print()
-    # for (a, _) in ucs.items():
-    #    print(f"c {a} {")
-
-
-    ###i = int(sys.argv[1])
-    ###ucs_file = list(open(sys.argv[2], "r").readlines())
-    ###ucs = {
-    ###    l[i]: int(l[i + 1])
-    ###    for l in map(
-    ###        lambda l: l.rstrip("\n").split(" ")[1:],
-    ###        filter(lambda l: l.startswith("c "), ucs_file),
-    ###    )
-    ###}
-    ###j = int(sys.argv[3])
-    ###ccg = {
-    ###    l[j]: int(l[j + 1])
-    ###    for l in map(
-    ###        lambda l: l.rstrip("\n").split(" ")[1:],
-    ###        filter(lambda l: l.startswith("c "), open(sys.argv[4], "r").readlines()),
-    ###    )
-    ###}
-
-    ###maps = {
-    ###    v_ucs: v_ccg
-    ###    for (k_ucs, v_ucs) in ucs.items()
-    ###    for (k_ccg, v_ccg) in ccg.items()
-    ###    if k_ucs == k_ccg
-    ###}
-
-    #### print(ucs_file[0], end="")
-    ###for line in filter(lambda l: not l.startswith("c "), ucs_file[1:]):
-    ###    for a in map(int, line.strip(" \n").split(" ")):
-    ###        if a > 0:
-    ###            print(maps[a], end=" ")
-    ###        else:
-    ###            print(f"-{maps[abs(a)]}", end=" ")
-    ###    print()
-    #### for (a, _) in ucs.items():
-    ####    print(f"c {a} {")
